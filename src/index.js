778edb041a86294b57abd715959cd74802a610e2
Saloni Subedi <subedisaloni2002@gmail.com>
2021-01-30 10:20:26 +0530

    JSX intro

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+/*--------------------JSX-------------------------- 
+const fname = "Saloni";
+const lname = "Subedi";
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <h2>Hi {fname + " " + lname}.</h2>
+    <p>Your lucky number is {Math.floor(Math.random()*10)+1}.</p>
+    <p>Copyright &copy; {year}.</p>
+  </div>,
+  document.getElementById("root")
+);*/
