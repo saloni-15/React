714db73c6d425392409f947633c73f2b2f606e17
Saloni Subedi <subedisaloni2002@gmail.com>
2021-01-30 11:45:27 +0530

    Attributes and Styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+/*Javascipt properties as attributes in JSX and styling
+
+Attributes name should be written in camelCase only as javascript not like in HTMl.
+
+eg. contenteditable --> contentEditable*/
+const image = "https://picsum.photos/200";
+ReactDOM.render(
+  <div>
+    <h1 className="heading" contentEditable="true" spellCheck="false">
+      My Favourite Foods
+    </h1>
+    <ul>
+      <li>Momos</li>
+      <li>Chicken</li>
+      <li>Pizza</li>
+    </ul>
+    <div className="size">
+      <img
+        alt="images"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOtUaoZrna8K7umWmYZVqUnnEhxktJl97CNw&usqp=CAU"
+      ></img>
+      <img
+        alt="images"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRByKfcEKCni41Me5skxEPt2hrRsC2449nvKA&usqp=CAU"
+      ></img>
+      <img
+        alt="images"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3sT26_BWoXpD4eYMA0PjxLTb5bOhCkyt1Gw&usqp=CAU"
+      ></img>
+    </div>
+    <img alt="images" src={image + "?grayscale"}></img>
+  </div>,
+  document.getElementById("root")
+);
+
 /*--------------------JSX-------------------------- 
 const fname = "Saloni";
 const lname = "Subedi";
